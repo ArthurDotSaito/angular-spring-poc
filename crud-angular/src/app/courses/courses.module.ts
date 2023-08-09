fa44279905ef6827745856612d696e3d0513e494
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-08 21:44:54 -0300

    chore: arrange angular-material imports

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,20 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [
-    CommonModule,
-    CoursesRoutingModule,
-    MatTableModule,
-    MatCardModule,
-    MatToolbarModule,
-  ],
+  imports: [CommonModule, CoursesRoutingModule, AppMaterialModule],
 })
 export class CoursesModule {}
