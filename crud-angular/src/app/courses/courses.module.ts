7a238a5f9a02cc624f41ead42f5bb6ccf31309ee
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-08 21:24:09 -0300

    feat: rendering course table component

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [CommonModule, CoursesRoutingModule],
+  imports: [CommonModule, CoursesRoutingModule, MatTableModule],
 })
 export class CoursesModule {}
--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
 
+  constructor() {}
+  ngOnInit(): void {}
 }
