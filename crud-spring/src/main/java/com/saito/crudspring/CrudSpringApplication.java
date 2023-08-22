e146b274892d4f5054b8e6d3d2149d7eccde32d9
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-22 20:27:25 -0300

    chore: adding Spring

---
+++ crud-spring/src/main/java/com/saito/crudspring/CrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.saito.crudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+}
