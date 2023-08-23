5d85cc0a2d0a3cd640ed97a207c60eccda2a71e6
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-23 00:02:44 -0300

    chore: adding a course model

---
+++ crud-spring/src/main/java/model/Course.java
@@ -0,0 +1,23 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 10, nullable = false)
+    private String category;
+}
