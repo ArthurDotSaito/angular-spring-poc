2383bf301bd370eb2f70c007d32b737e9e457abe
Arthur Saito <arthur_saito1998@hotmail.com>
2023-08-23 23:03:04 -0300

    fix: fixing _id property on Course Entity

--- crud-spring/src/main/java/com/saito/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/saito/crudspring/model/Course.java
@@ -1,5 +1,7 @@
 package com.saito.crudspring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,6 +15,7 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
