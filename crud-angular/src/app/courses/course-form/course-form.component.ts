23abdb1f44c5ad395ba075d4f91777d96855540d
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-03 21:23:27 -0300

    feat: implementing formGroup

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.css']
+  styleUrls: ['./course-form.component.css'],
 })
-export class CourseFormComponent {
-
+export class CourseFormComponent implements OnInit {
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null],
+    });
+  }
+  ngOnInit(): void {}
 }
--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
+import { CourseFormComponent } from './course-form/course-form.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { CourseFormComponent } from './course-form/course-form.component';
 
 @NgModule({
   declarations: [CoursesComponent, CourseFormComponent],
@@ -14,6 +15,7 @@ import { CourseFormComponent } from './course-form/course-form.component';
     CoursesRoutingModule,
     AppMaterialModule,
     SharedModule,
+    ReactiveFormsModule,
   ],
 })
 export class CoursesModule {}
